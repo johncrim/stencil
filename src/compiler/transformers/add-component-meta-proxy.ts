2edff373443d1a32ce7a81b1cb27f3ee07d9cc5f
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-02-17 12:15:05 -0500

    doc createComponentMetadataProxy

--- src/compiler/transformers/add-component-meta-proxy.ts
+++ src/compiler/transformers/add-component-meta-proxy.ts
@@ -19,9 +19,18 @@ const addComponentMetadataProxy = (compilerMeta: d.ComponentCompilerMeta) => {
 };
 
 /**
+ * Create a call expression for wrapping a component in a proxy. This call expression takes a form:
+ * ```ts
+ * PROXY_CUSTOM_ELEMENT(ComponentClassName, Metadata);
+ * ```
+ * where
+ * - `PROXY_CUSTOM_ELEMENT` is a Stencil internal identifier that will be replaced with the name of the actual function
+ * name at compile name
+ * - `ComponentClassName` is the name Stencil component's class
+ * - `Metadata` is the compiler metadata associated with the Stencil component
  *
- * @param compilerMeta
- * @returns
+ * @param compilerMeta compiler metadata associated with the component to be wrapped in a proxy
+ * @returns the generated call expression
  */
 export const createComponentMetadataProxy = (compilerMeta: d.ComponentCompilerMeta): ts.CallExpression => {
   const compactMeta: d.ComponentRuntimeMetaCompact = formatComponentRuntimeMeta(compilerMeta, true);
